2e2211d9fd1c9358b19e1f9cd90880318da5ac19
merge8266 <magicntuli6@gmail.com>
2026-05-06 01:15:15 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Jacob's Craft Interiors",
+  description: "Cabinets, Tiling, Interior Finishing",
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
